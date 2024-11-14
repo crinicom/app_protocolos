b90eebf7cf60303f277e911fb600ef83480d6fec
Cristian Menajovsky <cristian.menajovsky@gmail.com>
2024-11-13 23:31:13 -0300

    cambio view

--- parser/views.py
+++ parser/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+""" from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.") """
+
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = ["uno", "dos", "tres"]
+    template = loader.get_template("templates/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
